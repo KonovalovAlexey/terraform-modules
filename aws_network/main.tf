45567205ccc52428b030bb03f6c75eae401866bd
Oleksii Konovalov <oleksii_konovalov@epam.com>
2021-04-28 11:13:23 +0300

    Add  myip

--- aws_network/main.tf
+++ aws_network/main.tf
@@ -9,7 +9,16 @@
 #
 
 #----------------------------------------------------------
-
+terraform {
+  backend "s3" {
+    bucket = "project-terraform-remote"
+    key = "dev/network/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+provider "aws" {
+  region = "eu-central-1"
+}
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
